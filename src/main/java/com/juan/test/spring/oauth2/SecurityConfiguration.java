bce4823ff0cc4b8ed9199f3c6b8d8410738904a5
jfcorugedo <jfcorugedo@gmail.com>
2015-10-02 10:29:31 +0200

    removes unused imports

--- src/main/java/com/juan/test/spring/oauth2/SecurityConfiguration.java
+++ src/main/java/com/juan/test/spring/oauth2/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
